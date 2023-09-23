429c5ee03e9f20e688125ff348bbaaab05f79739
Anna <annnkaosipova@gmail.com>
2023-09-24 00:47:44 +0700

    create login form

---
+++ Cinema/Cinema/Helpers/FooterHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cinema.Helpers
+{
+    public static class FooterHelper
+    {
+        public static MvcHtmlString Footer(this HtmlHelper htmlHelper, params string[] items)
+        {
+            /*<footer><p>content</p></footer>*/
+            var footer = new TagBuilder(tagName: "footer");
+
+            foreach (var item in items)
+            {
+                var p = new TagBuilder(tagName: "p");
+                p.SetInnerText(item);
+                footer.InnerHtml += p.ToString();
+            }
+            return new MvcHtmlString(footer.ToString());
+        }
+    }
+}
---
+++ Cinema/Cinema/Models/LoginModel.cs
@@ -0,0 +1,21 @@
+﻿using Cinema.Binders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cinema.Models
+{
+    //[ModelBinder(typeof(LoginBinder))]
+    public class LoginModel
+    {
+        [Required]
+        [MinLength(4, ErrorMessage = "Login should be more than 4 symbols")]
+        public string Login { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
